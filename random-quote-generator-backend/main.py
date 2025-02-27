91bb67a41543a58a950dbf986ff023dc638e7de5
MaeseJoe <dracojoy@gmail.com>
2025-02-27 09:24:52 +0100

    TSK-001_first-version-ended

--- random-quote-generator-backend/main.py
+++ random-quote-generator-backend/main.py
@@ -1,14 +1,24 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import configparser
+import google.generativeai as genai
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+config = configparser.ConfigParser()
+config.read("random-quote-generator.properties")
 
-@app.route('/api/data', methods=['GET'])
-def get_data():
-    data = {
-        "mensaje": "Hello World",
-        "status": "Success"
-    }
-    return jsonify(data)
+API_KEY = config.get("DEFAULT", "API_KEY")
+
+genai.configure(api_key=API_KEY)
+
+@app.route('/get-quote', methods=['POST'])
+def get_happy_quote():
+    quote_type = request.get_json().get("type")
+    model = genai.GenerativeModel(model_name="gemini-2.0-flash")
+
+    quote = model.generate_content("Generate one random {} phrase".format(quote_type)).text[0:-1]
+    return jsonify({ "quote": quote })
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="localhost", port=8080 ,debug=True)
--- random-qutoe-generator-frontend/src/App.tsx
+++ random-qutoe-generator-frontend/src/App.tsx
@@ -1,35 +1,105 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css"
+import { Button, Flex, Group, Space, Stack, Title } from "@mantine/core"
+import { IconSun, IconMoodSad, IconRocket, IconHeart, IconHelpOctagon } from "@tabler/icons-react"
+import { useState } from "react";
+
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [text, setText] = useState("");
+    const [color, setColor] = useState("white");
+    const [isGettingQuote, setIsGettingQuote] = useState(false);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const getColor = (type: string): string => {
+        switch(type){
+            case "happy":
+                return "#FFD700";
+            case "sad":
+                return "#4682B4";
+            case "motivational":
+                return "#32CD32";
+            case "love":
+                return "#FF4500";
+            case "philosophical":
+                return "#800080";
+            default:
+                return "white";
+        }
+    }
+
+    const getQuote = (type: string) => {
+        if(isGettingQuote) {
+            return;
+        }
+        setIsGettingQuote(true);
+        const data = {
+            type: type
+        }
+        const requestOptions = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        }
+        fetch(`${BASE_URL}/get-quote`, requestOptions)
+        .then((response) => response.json())
+        .then((data) => {
+            setText(data.quote);
+            setColor(getColor(type));
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+        .finally(() => {
+            setIsGettingQuote(false);
+        });
+        
+    }
+
+    return (
+        <>
+            <Flex h="100vh" w="100vw" top={0} left={0} pos="absolute" direction="column" align="center">
+                <Space h="40vh"/>
+                <Title h="5vh" c={color} order={1} className="quote">{text}</Title>
+                <Space h="15vh"/>
+                <Title order={2}>Select your type of quote</Title>
+                <Space h="xl"/>
+                <Group>
+                    <Button h="15vh" w="8vw" variant="default" onClick={() => getQuote("happy")}>
+                        <Stack align="center">
+                            <Title order={4}>Happy</Title>
+                            <IconSun size={50} stroke={1.5}/>
+                        </Stack>
+                    </Button>
+                    <Button h="15vh" w="8vw" variant="default" onClick={() => getQuote("sad")}>
+                        <Stack align="center">
+                            <Title order={4}>Sad</Title>
+                            <IconMoodSad size={50} stroke={1.5}/>
+                        </Stack>
+                    </Button>
+                    <Button h="15vh" w="8vw" variant="default" onClick={() => getQuote("motivational")}>
+                        <Stack align="center">
+                            <Title order={4}>Motivational</Title>
+                            <IconRocket size={50} stroke={1.5}/>
+                        </Stack>
+                    </Button>
+                    <Button h="15vh" w="8vw" variant="default" onClick={() => getQuote("love")}>
+                        <Stack align="center">
+                            <Title order={4}>Love</Title>
+                            <IconHeart size={50} stroke={1.5}/>
+                        </Stack>
+                    </Button>
+                    <Button h="15vh" w="8vw" variant="default" onClick={() => getQuote("philosophical")}>
+                        <Stack align="center">
+                            <Title order={4}>Philosophical</Title>
+                            <IconHelpOctagon size={50} stroke={1.5}/>
+                        </Stack>
+                    </Button>
+                </Group>
+            </Flex>
+        </>
+    )
 }
 
 export default App
